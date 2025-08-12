79e1ad2c84f2507fd8fb21fb52eaeb3ab83d79a5
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-08-12 09:03:32 +0530

    Add files via upload

---
+++ Sieve_Of_Eratosthenes.java
@@ -0,0 +1,19 @@
+//package Recursion_MONUBHAIYA;
+//
+//public class Sieve_Of_Eratosthenes {
+//    public static void main(String[] args) {
+//
+//    }
+//    //jis index pe 0 hai us index ko prime number bolenge
+//    public static int PrimeSieve(int n){
+//        int[] ans=new int[n];
+//        ans[0]=ans[1]=1;
+//        for(int i=2;i*i<=ans.length;i++){
+//            if(ans[i]==0){ //i prime hai
+//                for(int j=2;i*j<ans.length;j++){
+//                    ans[i*j]=1;
+//                }
+//            }
+//        }
+//    }
+//}
