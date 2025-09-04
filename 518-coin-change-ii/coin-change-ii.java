a43936fddc8b409ff4cc90f4562900beb4a58b89
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-09-04 12:16:46 +0530

    Time: 12 ms (26.01%) | Memory: 49.3 MB (31.98%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,29 +1,27 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp = new int[amount+1][coins.length];
-        for(int[] a : dp){
-            Arrays.fill(a, -1);
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 1;
         }
-        return CoinChange(amount, coins, 0, dp);
-    }
-    public static int CoinChange(int amount, int[] coins, int i, int[][] dp){
-        if(amount == 0){
-            return 1;
-        }
-        if(i == coins.length){
-            return 0;
-        }
-        if(dp[amount][i] != -1){
-            return dp[amount][i];
+
+      
+        for (int i = 1; i < dp.length; i++) { // coins
+            for (int am = 1; am < dp[0].length; am++) { // amount
+                int inc = 0, exc = 0;
+                
+               
+                if (am >= coins[i - 1]) {
+                    inc = dp[i][am - coins[i - 1]];
+                }
+            
+                exc = dp[i - 1][am];
+                
+                dp[i][am] = inc + exc;
+            }
         }
         
-        int inc = 0;
-        int exc = 0;
-
-        if(amount>=coins[i]){
-            inc = CoinChange(amount - coins[i],coins, i, dp);
-        }
-        exc = CoinChange(amount, coins, i+1, dp);
-        return dp[amount][i] = inc + exc;
+        return dp[dp.length - 1][dp[0].length - 1];
     }
 }
