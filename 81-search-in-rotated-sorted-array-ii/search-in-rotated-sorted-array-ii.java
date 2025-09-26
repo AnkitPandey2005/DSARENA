dd433d7caac874e821c30d06dabbaf2c92be57f3
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-09-26 14:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (6.07%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,61 @@
+public class Solution {
+    public boolean search(int[] arr, int target) {
+        int pivot = findpivot(arr);
+        if (pivot == -1) {
+            return binarySearch(arr, target, 0, arr.length - 1);
+        }
+        if (arr[pivot] == target) {
+            return true;
+        }
+        if (target >= arr[0]) {
+            return binarySearch(arr, target, 0, pivot - 1);
+        } else {
+            return binarySearch(arr, target, pivot + 1, arr.length - 1);
+        }
+    }
+
+    static boolean binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static int findpivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            
+            if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            } else if (arr[start] <= arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
