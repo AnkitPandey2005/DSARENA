81efda12997eace4e66220556330909293dc9f48
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-09-30 00:47:53 +0530

    Time: 5 ms (99.40%) | Memory: 46.1 MB (40.61%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,26 +3,16 @@ class Solution {
         int n=matrix.length;
         int m=matrix[0].length;
 
-        if (n == 0) return false; // Check for empty matrix
+        int row=0;
+        int col=m-1;
 
-        for (int i = 0; i < n; i++) {
-            if (BS(matrix[i], target)) {
+        while(row<n && col>=0){
+            if(matrix[row][col]==target){
                 return true;
             }
+            else if(target>matrix[row][col]) row++;
+            else col--;
         }
         return false;
-    }
-    public static boolean BS(int[] arr,int target){
-        int low=0;
-        int high=arr.length-1;
-
-        while(low<=high){
-            int mid=(low+high)/2;
-
-            if(arr[mid]==target) return true;
-            else if(target>arr[mid]) low=mid+1;
-            else high=mid-1;
-        }
-        return false;
-    }
+   }
 }
