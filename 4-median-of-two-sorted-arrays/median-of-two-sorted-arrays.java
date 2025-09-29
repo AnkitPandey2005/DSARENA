599e6feb44ae15be1bbd03150e6e8a1bb8e6bb26
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-09-29 12:02:36 +0530

    Time: 1 ms (100.00%) | Memory: 45.9 MB (85.33%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,33 +1,33 @@
-import java.util.ArrayList;
-
 class Solution {
-    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int n1 = arr1.length;
-        int n2 = arr2.length;
-        int i = 0, j = 0;
-        ArrayList<Integer> ans = new ArrayList<>();
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        if (a.length > b.length) {
+            int[] temp = a; a = b; b = temp;
+        }
+        int lb = 0, ub = a.length;
+        int t = a.length + b.length;
+        int hf = (t + 1) / 2;
 
-        while (i < n1 && j < n2) {
-            if (arr1[i] < arr2[j]) {
-                ans.add(arr1[i++]);
+        while (lb <= ub) {
+            int a1 = lb + (ub - lb) / 2;
+            int b1 = hf - a1;
+
+            int aleft  = (a1 == 0) ? Integer.MIN_VALUE : a[a1 - 1];
+            int aright = (a1 == a.length) ? Integer.MAX_VALUE : a[a1];
+            int bleft  = (b1 == 0) ? Integer.MIN_VALUE : b[b1 - 1];
+            int bright = (b1 == b.length) ? Integer.MAX_VALUE : b[b1];
+
+            if (aleft <= bright && bleft <= aright) {
+                if (t % 2 == 0) {
+                    return (Math.max(aleft, bleft) + Math.min(aright, bright)) / 2.0;
+                } else {
+                    return Math.max(aleft, bleft);
+                }
+            } else if (aleft > bright) {
+                ub = a1 - 1;
             } else {
-                ans.add(arr2[j++]);
+                lb = a1 + 1;
             }
         }
-
-        while (i < n1) {
-            ans.add(arr1[i++]);
-        }
-
-        while (j < n2) {
-            ans.add(arr2[j++]);
-        }
-
-        int n = n1 + n2;
-        if (n % 2 == 1) {
-            return ans.get(n / 2);
-        } else {
-            return (ans.get(n / 2) + ans.get(n / 2 - 1)) / 2.0;
-        }
+        return 0.0; 
     }
 }
