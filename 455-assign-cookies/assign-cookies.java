33c9d547f4de68dd3d45b7bfc161d8443796314b
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-10-02 11:38:08 +0530

    Time: 9 ms (98.96%) | Memory: 48.8 MB (35.75%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,18 +1,21 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        //student -->g
-        //cookie--->s
         Arrays.sort(g);
         Arrays.sort(s);
-        int i=0;
-        int j=0;
 
-        while(i<g.length && j<s.length){
-            if(s[j]>=g[i]){
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            // If the cookie satisfies the student's greed
+            if (s[j] >= g[i]) {
                 i++;
             }
+            // Move to next cookie in both cases
             j++;
         }
+
+        // Number of students satisfied is equal to studentIndex
         return i;
     }
 }
