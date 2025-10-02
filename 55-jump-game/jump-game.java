d0fa03ed3a207f45e827449c7ffd527a7e4e7e1f
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-10-02 19:22:57 +0530

    Time: 3 ms (39.66%) | Memory: 46.3 MB (7.89%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,11 @@
 class Solution {
-    public boolean canJump(int[] arr) {
-        int reach=0;
-        for(int i=0;i<=reach;i++){
-            reach=Math.max(reach,arr[i]+i);
-            if(reach>=arr.length-1){
-                return true;
-            }
+    public boolean canJump(int[] nums) {
+        int maxIndex=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxIndex) return false; //max you able to reach is maxindex how did you greater than maxindex
+
+            maxIndex=Math.max(maxIndex,i+nums[i]);
         }
-        return false;
+        return true;
     }
 }
