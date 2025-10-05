a7b74ea4c9d7b8ec44e11cb15229f8783cab6b1a
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-10-05 19:21:53 +0530

    Time: 23 ms (74.41%) | Memory: 40.9 MB (23.44%) - LeetSync

---
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -0,0 +1,33 @@
+class Solution {
+    int cnt = 0;
+    
+    public int countArrangement(int n) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+        check(arr, 0);
+        return cnt;
+    }
+    
+    public void check(int[] arr, int j) {
+        if (j == arr.length) {
+            cnt++;
+            return;
+        }
+        for (int i = j; i < arr.length; i++) {
+            swap(arr, i, j);
+            // check criteria
+            if (arr[j] % (j + 1) == 0 || (j + 1) % arr[j] == 0) {
+                check(arr, j + 1);
+            }
+            swap(arr, i, j); // backtrack
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
