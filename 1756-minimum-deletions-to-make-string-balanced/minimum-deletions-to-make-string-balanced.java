4b915a5e88d333ea303a36c8615c60f4bb8514be
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-10-06 22:46:23 +0530

    Time: 103 ms (17.78%) | Memory: 45.5 MB (71.48%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,15 +1,20 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int numb=0;
-        int mindel=0;
+        Stack<Character> st = new Stack<>();
+        int cnt = 0;
 
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='a'){
-                mindel=Math.min(mindel+1,numb);
-            }else{
-                numb++;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            // If current char is 'a' and top of stack is 'b', remove that 'b' (count as deletion)
+            if (!st.isEmpty() && ch == 'a' && st.peek() == 'b') {
+                st.pop();
+                cnt++;
+            } else {
+                st.push(ch);
             }
         }
-        return mindel;
+
+        return cnt;
     }
 }
