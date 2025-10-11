86cf5f7c20919a424fa2243bb6683c4dcb3d905b
AnkitPandey2005 <135981529+AnkitPandey2005@users.noreply.github.com>
2025-10-11 15:26:38 +0530

    Time: 38 ms (89.66%) | Memory: 78.5 MB (31.15%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
@@ -1,66 +1,61 @@
 class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        int m = flowers.length;
-        int n = people.length;
+         int m=flowers.length;
+         int n=people.length;
 
-        int[] result = new int[n];
+         int[] result=new int[n];
 
-        int[] start_time = new int[m];
-        int[] end_time = new int[m];
+         int[] start_time=new int[m];
+         int[] end_time=new int[m];
 
-        for (int i = 0; i < m; i++) {
-            start_time[i] = flowers[i][0];
-            end_time[i] = flowers[i][1];
-        }
+         for(int i=0;i<m;i++){
+            start_time[i]=flowers[i][0];
+            end_time[i]=flowers[i][1];
+         }
 
-        Arrays.sort(start_time);
-        Arrays.sort(end_time);
+         Arrays.sort(start_time);
+         Arrays.sort(end_time);
 
-        for (int i = 0; i < n; i++) {
-            int bloomed_already = binarySearchUpperBound(start_time, people[i]);
-            int died_already = binarySearchLowerBound(end_time, people[i]);
-            result[i] = bloomed_already - died_already;
-        }
-
-        return result;
+         for(int i=0;i<n;i++){
+            int bloomed=upperbound(start_time,people[i]);
+            int died=lowerbound(end_time,people[i]);
+            result[i]=bloomed-died;
+         }
+         return result;
     }
 
-    private int binarySearchUpperBound(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length-1;
-        int result = -1;
+    public static int lowerbound(int[] arr,int target){
+        int low=0;
+        int high=arr.length-1;
+        int ans=arr.length;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if(arr[mid] > target) {
-                result = mid;
-                right = mid-1;
-            } else {
-                left = mid+1;
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(arr[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-
-        return result == -1 ? arr.length : result;
+        return ans;
     }
+    public static int upperbound(int[] arr,int target){
+        int low=0;
+        int high=arr.length-1;
+        int ans=arr.length;
 
-    private int binarySearchLowerBound(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length-1;
-        int result = -1;
+        while(low<=high){
+            int mid=(low+high)/2;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if(arr[mid] < target) {
-                left = mid+1;
-            } else {
-                result = mid;
-                right = mid-1;
+            if(arr[mid]>target){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
-            
         }
-  
-        return result == -1 ? arr.length : result;
-    }
+        return ans;
+    }   
 }
