0552130cba630587c908df5cef5852d78124dad8
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2025-12-26 12:34:38 +0530

    Time: 16 ms (37.86%) | Memory: 48.6 MB (27.01%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+        // Traverse 2*N (2 full passes for circular)
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int idx = i % n;
+            while (!st.isEmpty() && st.peek() <= nums[idx]) {
+                st.pop();
+            }
+            res[idx] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums[idx]);
+        }
+        return res;
+    }
+}
