decd9d6c82bae06fd7883b9b3b272eae8748e857
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-01-06 10:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (13.86%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -3,9 +3,8 @@ class Solution {
         int a=0;
         int b=1;
         int c=1;
-        int d=1;
         for(int i=1;i<=n;i++){
-            d=a+b+c;
+            int d=a+b+c;
             a=b;
             b=c;
             c=d;
