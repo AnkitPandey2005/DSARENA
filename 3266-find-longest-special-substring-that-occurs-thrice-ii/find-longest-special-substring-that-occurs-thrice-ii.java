caff3034086f313a3d3a3a12c8428da1400ebc6f
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-01-20 17:10:35 +0530

    Time: 165 ms (38.98%) | Memory: 284.6 MB (9.32%) - LeetSync

---
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumLength(String s) {
+        int[][] pre = new int[26][s.length()+1];
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            int j=i+0;
+            
+            while(j<s.length() && c==s.charAt(j)){
+                pre[c-'a'][j-i]++;
+                j++;
+            }
+            
+            i=j-1;
+        }
+        int ans=-1;
+        for(int[] arr:pre){
+            int prev=0;
+            for(int i=s.length()-1;i>-1;i--){
+                arr[i]+=prev;
+                prev=arr[i]+0;
+                if(arr[i]>=3)ans=Math.max(ans,i+1);
+            }
+        }
+        return ans;
+    }
+}
