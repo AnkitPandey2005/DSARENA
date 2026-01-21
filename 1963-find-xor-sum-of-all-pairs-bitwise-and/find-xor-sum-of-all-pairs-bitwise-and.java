9d163dfcfca3ee7977387b5abba4ff7368123b42
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-01-21 12:12:42 +0530

    Time: 1 ms (98.04%) | Memory: 87.9 MB (10.78%) - LeetSync

---
+++ 1963-find-xor-sum-of-all-pairs-bitwise-and/find-xor-sum-of-all-pairs-bitwise-and.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getXORSum(int[] arr1, int[] arr2) {
+        int xor1 = 0;
+        int xor2 = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            xor1 ^= arr1[i];
+        }
+        for (int j = 0; j < arr2.length; j++) {
+            xor2 ^= arr2[j];
+        }
+        return xor1 & xor2;
+    }
+}
