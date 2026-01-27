7b1e383b307869f89fc61a3d5ee30e79235a6b55
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-01-28 01:58:38 +0530

    Time: 1 ms (60.59%) | Memory: 47.4 MB (96.31%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int trap(int[] height) {
-       int left = 0, right = height.length - 1;
-        int leftMax = 0, rightMax = 0;
-        int total = 0;
-        
-        while (left < right) {
-            if (height[left] < height[right]) {
-                if (height[left] >= leftMax) {
-                    leftMax = height[left];
-                } else {
-                    total += leftMax - height[left];
-                }
-                left++;
-            } else {
-                if (height[right] >= rightMax) {
-                    rightMax = height[right];
-                } else {
-                    total += rightMax - height[right];
-                }
-                right--;
-            }
-        }
-        return total; 
+    public int trap(int[] arr) {
+        int n = arr.length;
+    int[] bound = new int[n]; 
+    
+    // Left to right: prefix max
+    bound[0] = arr[0];
+    for(int i = 1; i < n; i++) {
+        bound[i] = Math.max(bound[i-1], arr[i]);
+    }
+    
+    // Right to left: take min with suffix max
+    int suffixMax = arr[n-1];
+    for(int i = n-2; i >= 0; i--) {
+        suffixMax = Math.max(suffixMax, arr[i+1]);
+        bound[i] = Math.min(bound[i], suffixMax);
+    }
+    
+    int total = 0;
+    for(int i = 0; i < n; i++) {
+        total += bound[i] - arr[i];
+    }
+    return total;
     }
 }
