04ad3e95c25f8743cbc614935db81f7f751cff46
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-01-29 19:11:53 +0530

    Time: 2 ms (50.46%) | Memory: 43.2 MB (49.39%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,18 +1,19 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         int n=tickets.length;
-        Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<n;i++){ //issme phele saare index queue m daal de
-            q.add(i);
-        }
         int time=0;
-        while(tickets[k]!=0){  //jb tk req wala indx 0 naa tb tk krte rho
-            int z=q.poll();  //start se element nekal
-            tickets[z]=tickets[z]-1;  //usko aek ticket buy krne k baad subtract phir uske peeche queue m add kr dege
-            if(tickets[z]!=0){
-                q.add(z);
+
+        while(tickets[k]!=0){
+            for(int i=0;i<n;i++){
+                if(tickets[i]!=0){
+                    tickets[i]--;
+                    time++;
+                }
+
+                if(tickets[k]==0){
+                    return time;
+                }
             }
-            time++;
         }
         return time;
     }
