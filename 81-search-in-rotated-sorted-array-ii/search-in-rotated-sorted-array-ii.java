16e096b2e83218ec5bd1e36659625ef15e5a3054
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-01-31 13:03:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (88.53%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,9 @@
 class Solution {
-    public boolean search(int[] arr, int target) {
-        int low=0;
-        int high=arr.length-1;
-
-        while(low<=high){
-            int mid=(low+high)/2;
-
-            if(arr[mid]==target) return true;  //if you are find it no need to elimination
-            //for duplicate's 
-            if(arr[low]==arr[mid]  && arr[mid]==arr[high]){
-                low++;
-                high--;
-                continue;
-            }
-            //left side sorted
-            if(arr[low]<=arr[mid]){
-                if(arr[low]<=target && target<=arr[mid]){
-                    high=mid-1;
-                }else{
-                    low=mid+1;
-                }
-            }
-            //right side sorted
-            else{
-                if(arr[mid]<=target && target<=arr[high]){
-                    low=mid+1;
-                }else{
-                    high=mid-1;
-                }
+    public boolean search(int[] nums, int target) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==target){
+                return true;
             }
         }
         return false;
