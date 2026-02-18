264899cd78cc09e47671823d5822a637fc5b0d49
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-02-18 23:29:39 +0530

    Time: 3 ms (100.00%) | Memory: 103.8 MB (38.96%) - LeetSync

---
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        int n=edges.length;
+        long[] scores=new long[n];
+
+        for(int i=0;i<n;i++){
+            scores[edges[i]]+=i;
+        }
+
+        long maxScore=Integer.MIN_VALUE;
+        int maxNode=0;
+
+        for(int i=0;i<n;i++){
+            if(scores[i]>maxScore){
+                maxScore=scores[i];
+                maxNode=i;
+            }
+        }
+        return maxNode;
+    }
+}
