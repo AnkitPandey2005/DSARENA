aa3269291293f20519305f496ba4cbce8e2aab02
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-03-25 20:40:32 +0530

    Time: 1 ms (44.03%) | Memory: 47.1 MB (20.39%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,19 +1,45 @@
+import java.util.*;
+
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-      DFS(image,sr,sc,color,image[sr][sc]);    
-      return image;
-    }
-    public static void DFS(int[][] image,int i,int j,int newcolor,int orgcolor){
-        //base case [3]
-        if(i<0 || j<0 ||i>=image.length || j>=image[0].length || image[i][j]!=orgcolor || image[i][j]==newcolor){
-            return;
+        int orgColor = image[sr][sc];
+
+        // Edge case
+        if (orgColor == color) return image;
+
+        int n = image.length;
+        int m = image[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr, sc});
+
+        image[sr][sc] = color;
+
+        int[][] directions = {
+            {-1, 0}, // top
+            {0, 1},  // right
+            {0, -1}, // left
+            {1, 0}   // bottom
+        };
+
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            int i = cell[0];
+            int j = cell[1];
+
+            for (int[] dir : directions) {
+                int ni = i + dir[0];
+                int nj = j + dir[1];
+
+                if (ni >= 0 && nj >= 0 && ni < n && nj < m 
+                    && image[ni][nj] == orgColor) {
+
+                    image[ni][nj] = color;
+                    q.offer(new int[]{ni, nj});
+                }
+            }
         }
 
-        image[i][j]=newcolor;
-
-        DFS(image,i-1,j,newcolor,orgcolor);//top
-        DFS(image,i,j+1,newcolor,orgcolor);//right
-        DFS(image,i,j-1,newcolor,orgcolor);//left
-        DFS(image,i+1,j,newcolor,orgcolor);//bottom
+        return image;
     }
 }
