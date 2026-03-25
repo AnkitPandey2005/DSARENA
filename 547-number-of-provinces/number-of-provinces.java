6f7a38fe60f82193e3f1b1496f7b87e7be34786b
Ankit Pandey <135981529+AnkitPandey2005@users.noreply.github.com>
2026-03-25 07:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (67.94%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,41 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-
-        // Step 1: Convert matrix to adjacency list
-        List<List<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-            for (int j = 0; j < n; j++) {
-                if (isConnected[i][j] == 1 && i != j) {
-                    adj.get(i).add(j);
-                }
-            }
-        }
-
-        // Step 2: DFS to count provinces
         boolean[] visited = new boolean[n];
         int provinces = 0;
 
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(i, adj, visited);
-                provinces++;
+                dfs(i, isConnected, visited);
+                provinces++;   // each DFS = 1 province
             }
         }
 
         return provinces;
     }
 
-    public void dfs(int curr, List<List<Integer>> adj, boolean[] visited) {
-        visited[curr] = true;
+    public void dfs(int city, int[][] isConnected, boolean[] visited) {
+        visited[city] = true;
 
-        for (int nbr : adj.get(curr)) {
-            if (!visited[nbr]) {
-                dfs(nbr, adj, visited);
+        for (int j = 0; j < isConnected.length; j++) {
+            if (isConnected[city][j] == 1 && !visited[j]) {
+                dfs(j, isConnected, visited);
             }
         }
     }
